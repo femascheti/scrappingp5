2268e6f043c25cf9147af0393c126409bb871d43
Fernanda Mascheti <43377020+femascheti@users.noreply.github.com>
2024-10-31 18:37:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def fetch_info_projetos(link):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--remote-debugging-port=9222')  # Necessário em alguns ambientes
+        options.add_argument('--window-size=1920x1080')
+        options.add_argument('--user-data-dir=/tmp/user-data')
+        options.add_argument('--data-path=/tmp/data-path')
+        options.add_argument('--homedir=/tmp')
+        options.add_argument('--disk-cache-dir=/tmp/cache-dir')
         driver = webdriver.Chrome(options=options)
 
         driver.get(user_sketches_url)
